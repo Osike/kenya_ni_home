0f72975535ebd387b818efcbab95a73eb2c3a0c3
Code_Tribe <82811609+Osike@users.noreply.github.com>
2025-07-14 07:29:35 +0300

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ChatPage from './pages/ChatPage';
+import ConstitutionPage from './pages/ConstitutionPage';
+import BillOfRightsPage from './pages/BillOfRightsPage';
+import AboutPage from './pages/AboutPage';
+import DisclaimerPage from './pages/DisclaimerPage';
+import LegalAidPage from './pages/LegalAidPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/constitution" element={<ConstitutionPage />} />
+          <Route path="/bill-of-rights" element={<BillOfRightsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/disclaimer" element={<DisclaimerPage />} />
+          <Route path="/legal-aid" element={<LegalAidPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
